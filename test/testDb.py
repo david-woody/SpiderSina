cc8e1cc596a0440677571134ec4d30457b43e915
woody <WoodyDing>
2016-06-16 00:28:22 +0800

    modify

--- test/testDb.py
+++ test/testDb.py
@@ -2,6 +2,7 @@ from sina import db_helper
 
 dbhelper = db_helper.DBclient()
 dbcollection = dbhelper.get_collection("user")
-for item in dbhelper.get_urls():
-    print item.__getitem__("url")
-dbhelper.removeUrl("http://weibo.com/u/3045049884?profile_ftype=1&is_all=1#_0")
+dbhelper.insert_one_doc(dbcollection,{"what":"fuck"})
+# for item in dbhelper.get_urls():
+#     print item.__getitem__("url")
+# dbhelper.removeUrl("http://weibo.com/u/3045049884?profile_ftype=1&is_all=1#_0")
